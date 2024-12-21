43d35c538b1d603bbf4c2662d399de7f53d55fb5
Anula <anna.adamiak@edu.uekat.pl>
2024-12-21 14:49:13 +0100

    Update version to 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='student-management-system11',
-    version='0.1.4',
+    version='0.1.6',
     author='Anna Adamiak',
     author_email='anna.adamiak@edu.uekat.pl',
     description='A simple student management system',
