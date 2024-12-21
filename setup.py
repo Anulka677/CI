c555d29b6881bac79606a6ed3cec1c01629df3fe
Anula <anna.adamiak@edu.uekat.pl>
2024-12-21 12:54:07 +0100

    CD

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='student-management-system11',
+    version='0.1.0',
+    author='Anna Adamiak',
+    author_email='anna.adamiak@edu.uekat.pl',
+    description='A simple student management system',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Anulka677/CD',
+    packages=find_packages(),
+    install_requires=open('requirements.txt').read().splitlines(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
